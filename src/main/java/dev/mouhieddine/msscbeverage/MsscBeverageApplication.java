285f28f6ed35203ba9533dbe170af506f467a6b7
BIRSAx2 <Mouhieddinesabir@gmail.com>
2021-01-30 19:02:12 +0100

    Initial commit

---
+++ src/main/java/dev/mouhieddine/msscbeverage/MsscBeverageApplication.java
@@ -0,0 +1,13 @@
+package dev.mouhieddine.msscbeverage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeverageApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MsscBeverageApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/mouhieddine/msscbeverage/MsscBeverageApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mouhieddine.msscbeverage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeverageApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
