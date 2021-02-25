b1a2aa74663d2a564b663df1219d47788f70e1c0
BIRSAx2 <Mouhieddinesabir@gmail.com>
2021-02-25 18:04:50 +0100

    Moved validation exception handler to a controller advice

--- src/main/java/dev/mouhieddine/msscdrinks/web/controller/DrinkController.java
+++ src/main/java/dev/mouhieddine/msscdrinks/web/controller/DrinkController.java
@@ -58,14 +58,4 @@ public class DrinkController {
     drinkService.deleteById(id);
   }
 
-  @ExceptionHandler(ConstraintViolationException.class)
-  public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e) {
-    List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
-
-    e.getConstraintViolations().forEach(constraintViolation -> {
-      errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage());
-    });
-
-    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-  }
 }
